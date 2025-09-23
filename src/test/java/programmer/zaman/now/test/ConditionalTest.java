8f02ed8ec7897ed0c97d34af3b44043014659367
kaiharvest <indradwiprabowo27@gmail.com>
2025-09-23 08:09:02 +0700

    test berdasarkan kondisi

--- src/test/java/programmer/zaman/now/test/ConditionalTest.java
+++ src/test/java/programmer/zaman/now/test/ConditionalTest.java
@@ -1,4 +1,44 @@
 package programmer.zaman.now.test;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
 public class ConditionalTest {
+
+    @Test
+    @EnabledOnOs({OS.WINDOWS})
+    public void testRunOnlyWindows() {
+
+    }
+
+    @Test
+    @DisabledOnOs({OS.WINDOWS})
+    public void testDisabledOnlyWindows() {
+
+    }
+
+    @Test
+    @EnabledOnJre({JRE.JAVA_8})
+    public void testRunJre() {
+
+    }
+
+    @Test
+    @DisabledOnJre({JRE.JAVA_11})
+    public void testDisabledJre()  {
+
+    }
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_17)
+    public void testEnabledJava11To17() {
+
+    }
+
+    @Test
+    @DisabledForJreRange(min = JRE.JAVA_11, max = JRE.JAVA_17)
+    public void testDisabledJava11To17() {
+
+    }
+
 }
