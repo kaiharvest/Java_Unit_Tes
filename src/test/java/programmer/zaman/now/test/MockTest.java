3052608dbb4882aff5cfc7187c04bfd2da207cc5
kaiharvest <indradwiprabowo27@gmail.com>
2025-09-24 08:33:22 +0700

    mocking test

---
+++ src/test/java/programmer/zaman/now/test/MockTest.java
@@ -0,0 +1,23 @@
+package programmer.zaman.now.test;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+public class MockTest {
+
+    @Test
+    void testMock() {
+        List<String> list = Mockito.mock(List.class);
+        Mockito.when(list.get(0)).thenReturn("Indra");
+        Mockito.when(list.get(100)).thenReturn("Dwi");
+
+        System.out.println(list.get(0));
+        System.out.println(list.get(100));
+
+        Mockito.verify(list, Mockito.times(1)).get(0);
+    }
+
+}
